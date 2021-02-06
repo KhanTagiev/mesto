93a7051d18f349099b9eda9fd9a6b5dddbf421f1
Khan Tagiev <xan.2011@yandex.ru>
2021-02-06 12:26:56 +0300

    feat: openPopups

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,22 @@
 let popup = document.querySelector('.popup')
-let openPopupBtn = document.querySelector('.profile__btn_edit')
-let closePopupBtn = popup.querySelector('.popup__btn_close')
-let formElement = popup.querySelector('.form')
-let nameInput = formElement.querySelector('.form__input_name')
-let jobInput = formElement.querySelector('.form__input_job')
-let saveFormBtn = formElement.querySelector('.form__btn_save')
+let popupProfile = document.querySelector('.popup_profile')
+let popupPhotoCard = document.querySelector('.popup_photo-card')
+//let popupPhoto = document.querySelector('.popup_photo')
+let openPopupProfileBtn = document.querySelector('.profile__btn_edit')
+let openPopupPhotoCardBtn = document.querySelector('.profile__btn_add')
+let closePopupProfileBtn = popupProfile.querySelector('.popup__btn_close')
+let closePopupPhotoCardBtn = popupPhotoCard.querySelector('.popup__btn_close')
+//let closePopupPhotoBtn = popupPhoto.querySelector('.popup__btn_close')
+let formElementProfile = popupProfile.querySelector('.form')
+let nameInput = formElementProfile.querySelector('.form__input_name')
+let jobInput = formElementProfile.querySelector('.form__input_job')
+let saveFormBtn = formElementProfile.querySelector('.form__btn_save')
 let profile = document.querySelector('.profile')
 let nameProfile = profile.querySelector('.profile__name')
 let jobProfile = profile.querySelector('.profile__job')
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+function togglePopup (pop) {
+  pop.classList.toggle('popup_opened');
 }
 
 function upProfileInfo() {
@@ -22,11 +28,26 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
-    togglePopup()
+    togglePopup(popupProfile)
  }
 
-openPopupBtn.addEventListener('click', togglePopup);
-openPopupBtn.addEventListener('click', upProfileInfo);
-closePopupBtn.addEventListener('click', togglePopup);
-formElement .addEventListener('submit', formSubmitHandler);
-
+openPopupProfileBtn.addEventListener('click',() => {
+  togglePopup(popupProfile)
+});
+openPopupProfileBtn.addEventListener('click', upProfileInfo);
+openPopupPhotoCardBtn.addEventListener('click',() => {
+  togglePopup(popupPhotoCard)
+});
+//openPopupPhotoBtn.addEventListener('click',() => {
+//  togglePopup(popupPhoto)
+//});
+closePopupProfileBtn.addEventListener('click',() => {
+  togglePopup(popupProfile)
+});
+closePopupPhotoCardBtn.addEventListener('click',() => {
+  togglePopup(popupPhotoCard)
+});
+//closePopupPhotoBtn.addEventListener('click',() => {
+//  togglePopup(popupPhoto)
+//});
+formElementProfile.addEventListener('submit', formSubmitHandler)
