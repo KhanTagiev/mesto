32106e22b0059556a7b94a11a8bf2d0a40cf1b1b
KhanTagiev <75221806+KhanTagiev@users.noreply.github.com>
2021-02-09 00:39:39 +0300

    Revert "Feat/photocards"

--- scripts/index.js
+++ scripts/index.js
@@ -1,153 +1,32 @@
-const initialCards = [
-  {
-    name: 'Владимир',
-    link: './images/vladimir.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: './images/ivanovo.jpg'
-  },
-  {
-    name: 'Великий Новгород',
-    link: './images/velikiy-novgorod.jpg'
-  },
-  {
-    name: 'Кострома',
-    link: './images/kostroma.jpg'
-  },
-  {
-    name: 'Волгоград',
-    link: './images/volgograd.jpg'
-  },
-  {
-    name: 'Красная Поляна',
-    link: './images/krasnaya-polyana.jpg'
-  }
-];
-
 let popup = document.querySelector('.popup')
-let openPopupProfileBtn = document.querySelector('.profile__btn_edit')
-let openPopupPhotoCardBtn = document.querySelector('.profile__btn_add')
-
-let popupProfile = document.querySelector('.popup_profile')
-let closePopupProfileBtn = popupProfile.querySelector('.popup__btn_close')
-let formElementProfile = popupProfile.querySelector('.form')
-let nameInput = formElementProfile.querySelector('.form__input_name')
-let jobInput = formElementProfile.querySelector('.form__input_job')
-
-let popupPhotoCard = document.querySelector('.popup_photo-card')
-let closePopupPhotoCardBtn = popupPhotoCard.querySelector('.popup__btn_close')
-let formElementPhotoCard = popupPhotoCard.querySelector('.form')
-let siteInput = formElementPhotoCard.querySelector('.form__input_site')
-let linkInput = formElementPhotoCard.querySelector('.form__input_url')
-
-let popupPhotoView = document.querySelector('.popup_photo-view')
-let photoViewImage = popupPhotoView.querySelector('.photo-view__image')
-let photoViewName = popupPhotoView.querySelector('.photo-view__name')
-let closePopupPhotoBtn = popupPhotoView.querySelector('.popup__btn_close')
-
+let openPopupBtn = document.querySelector('.profile__edit-btn')
+let closePopupBtn = popup.querySelector('.popup__close-btn')
+let formElement = popup.querySelector('.form')
+let nameInput = formElement.querySelector('.form__input_name')
+let jobInput = formElement.querySelector('.form__input_job')
+let saveFormBtn = formElement.querySelector('.form__save-btn')
 let profile = document.querySelector('.profile')
 let nameProfile = profile.querySelector('.profile__name')
 let jobProfile = profile.querySelector('.profile__job')
-let photoCardsContainer = document.querySelector('.photo-cards__container')
-let template = document.querySelector('.template')
 
-function togglePopup (pop) {
-  pop.classList.toggle('popup_opened');
-}
-
-function upProfileInfo() {
+function openPopup() {
+  popup.classList.add('popup_opened')
   nameInput.value = nameProfile.textContent
   jobInput.value = jobProfile.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened')
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
-    jobProfile.textContent = jobInput.value;
-    togglePopup(popupProfile)
-}
+    jobProfile.textContent = jobInput.value
+    closePopup()
+ }
 
-function getCard(item) {
-  const newCard = template.content.cloneNode(true);
-  const imgEl = newCard.querySelector('.photo-card__image');
-  const nameEl = newCard.querySelector('.photo-card__name');
-  const deleteCardBtn = newCard.querySelector('.photo-card__btn_delete');
-  const likeCardBtn = newCard.querySelector('.photo-card__btn_like');
-  const openPopupPhotoViewBtn = newCard.querySelector('.photo-card__image');
+openPopupBtn.addEventListener('click', openPopup)
+closePopupBtn.addEventListener('click', closePopup)
+formElement .addEventListener('submit', formSubmitHandler)
 
-  imgEl.src = item.link;
-  imgEl.alt = item.name;
-  nameEl.textContent = item.name;
-
-  deleteCardBtn.addEventListener('click', deleteCard);
-  likeCardBtn.addEventListener('click', likeCard);
-  openPopupPhotoViewBtn.addEventListener('click',() => {
-  photoViewName.textContent = item.name;
-  photoViewImage.src = item.link;
-  photoViewImage.alt = item.name;
-  });
-  openPopupPhotoViewBtn.addEventListener('click',() => {
-    togglePopup(popupPhotoView)
-  });
-
-  return newCard;
-}
-
-function formCardHandler (evt) {
-  evt.preventDefault();
-
-  const nameCard = siteInput.value;
-  const linkCard = linkInput.value;
-  const photoCard = getCard({name: nameCard, link: linkCard});
-
-  photoCardsContainer.prepend(photoCard);
-  siteInput.value = '';
-  linkInput.value = '';
-
-  togglePopup(popupPhotoCard);
-}
-
-function renderCards() {
-  const cards = initialCards.map(getCard);
-  photoCardsContainer.append(...cards);
-}
-
-function deleteCard(event) {
-  const targetEl = event.target;
-  const targetItem = targetEl.closest('.photo-card');
-  targetItem.remove();
-}
-
-function likeCard(event) {
- const targetEl = event.target;
- targetEl.classList.toggle('photo-card__btn_like_active');
-}
-
-function cleanPopupPhotoView() {
-  photoViewName.textContent = '';
-  photoViewImage.src = '';
-  photoViewImage.alt = '';
-}
-
-renderCards();
-
-openPopupProfileBtn.addEventListener('click',() => {
-  togglePopup(popupProfile)
-});
-openPopupProfileBtn.addEventListener('click', upProfileInfo);
-openPopupPhotoCardBtn.addEventListener('click',() => {
-  togglePopup(popupPhotoCard)
-});
-closePopupProfileBtn.addEventListener('click',() => {
-  togglePopup(popupProfile)
-});
-closePopupPhotoCardBtn.addEventListener('click',() => {
-  togglePopup(popupPhotoCard)
-});
-closePopupPhotoBtn.addEventListener('click',() => {
-  togglePopup(popupPhotoView)
-});
-closePopupPhotoBtn.addEventListener('click',cleanPopupPhotoView);
-formElementProfile.addEventListener('submit', formSubmitHandler);
-formElementPhotoCard.addEventListener('submit', formCardHandler);
