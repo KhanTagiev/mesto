7e4f814019abff9b78cac85295c4d141ca5720a9
Khan Tagiev <xan.2011@yandex.ru>
2021-02-24 16:05:09 +0300

    refactor: universalization enableValidation function

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,58 +3,65 @@ const hasInvalidInput = (inputList) => {
     return !inputElement.validity.valid;
   })
 }
-const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement, buttonDisabledClass) => {
   if (hasInvalidInput(inputList)) {
     buttonElement.setAttribute('disabled', true);
-    buttonElement.classList.add('form__btn_disabled');
+    buttonElement.classList.add(buttonDisabledClass);
   } else {
     buttonElement.removeAttribute('disabled');
-    buttonElement.classList.remove('form__btn_disabled');
+    buttonElement.classList.remove(buttonDisabledClass);
   }
 }
 
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, inputErrorSelector, spanErrorSelector) => {
   const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
-  inputElement.classList.add('form__input_type_error');
+  inputElement.classList.add(inputErrorSelector);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add('form__input-error_active');
+  errorElement.classList.add(spanErrorSelector);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, inputErrorSelector, spanErrorSelector) => {
   const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
-  inputElement.classList.remove('form__input_type_error');
-  errorElement.classList.remove('form__input-error_active');
+  inputElement.classList.remove(inputErrorSelector);
+  errorElement.classList.remove(spanErrorSelector);
   errorElement.textContent = '';
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, inputErrorSelector,spanErrorSelector) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, inputErrorSelector, spanErrorSelector);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, inputErrorSelector, spanErrorSelector);
   }
 };
 
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-  const buttonElement = formElement.querySelector('.form__btn');
-  toggleButtonState(inputList, buttonElement)
+const setEventListeners = (formElement, inputSelector, buttonSelector, buttonDisabledClass, inputErrorSelector, spanErrorSelector) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  const buttonElement = formElement.querySelector(buttonSelector);
+  toggleButtonState(inputList, buttonElement, buttonDisabledClass)
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
+      checkInputValidity(formElement, inputElement, inputErrorSelector, spanErrorSelector);
+      toggleButtonState(inputList, buttonElement, buttonDisabledClass);
     });
   });
 };
 
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.form'));
+const enableValidation = ({formSelector, inputSelector, buttonSelector, buttonDisabledClass, inputErrorSelector,spanErrorSelector}) => {
+  const formList = Array.from(document.querySelectorAll(formSelector));
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', function (evt) {
       evt.preventDefault();
     });
-    setEventListeners(formElement);
+    setEventListeners(formElement, inputSelector, buttonSelector, buttonDisabledClass, inputErrorSelector, spanErrorSelector);
   });
 };
 
-enableValidation();
+enableValidation({
+  formSelector:'.form',
+  inputSelector:'.form__input',
+  buttonSelector: '.form__btn',
+  buttonDisabledClass: 'form__btn_disabled',
+  inputErrorSelector: 'form__input_type_error',
+  spanErrorSelector: 'form__input-error_active'
+});
